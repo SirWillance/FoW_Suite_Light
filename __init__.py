11c4873fbc080d20f7bd180572ed6ffb6b3b3405
SirWillance <SirWillance@gmail.com>
2025-02-25 19:09:32 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
-WEB_DIRECTORY = os.path.join(EXTENSION_ROOT, "frontend")  # Point to js/
-
-from .backend.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
